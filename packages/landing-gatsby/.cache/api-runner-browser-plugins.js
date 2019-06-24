47b85f422e10dcc7e4463ff401572d537cc3ce62
Edgar Mac <edbarrios5@hotmail.com>
2019-06-23 23:25:29 -0500

    initial commit

---
+++ packages/landing-gatsby/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/www/sites/SuperProps - React Next Landing Page Templates/SuperProps/packages/landing-gatsby/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
